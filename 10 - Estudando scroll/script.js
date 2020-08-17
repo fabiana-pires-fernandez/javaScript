75512d12eda92113ef9e39c6f85c0110c0828ed2
fabiana-pires-fernandez <fabi.nandez@hotmail.com>
2020-08-17 12:12:48 -0300

    Estudando scroll

---
+++ 10 - Estudando scroll/script.js
@@ -0,0 +1,53 @@
+<<<<<<< HEAD
+function primeiroScroll() {
+	window.scrollTo(0, 0);
+}
+
+function segundoScroll() {
+	window.scrollTo(300, 750);
+}
+
+function terceiroScroll() {
+	window.scrollTo(0, 1450);
+}
+
+let quarta = document.getElementById("quarta-section");
+let quinta = document.getElementById("quinta-section");
+
+document.addEventListener("scroll", function( ){
+	if (document.documentElement.scrollTop > 1400) {
+		quarta.classList.add("pink");
+	}
+	
+	if (document.documentElement.scrollTop > 2100) {
+		quinta.classList.add("image");
+	}
+});
+
+=======
+function primeiroScroll() {
+	window.scrollTo(0, 0);
+}
+
+function segundoScroll() {
+	window.scrollTo(300, 750);
+}
+
+function terceiroScroll() {
+	window.scrollTo(0, 1450);
+}
+
+let quarta = document.getElementById("quarta-section");
+let quinta = document.getElementById("quinta-section");
+
+document.addEventListener("scroll", function( ){
+	if (document.documentElement.scrollTop > 1400) {
+		quarta.classList.add("pink");
+	}
+	
+	if (document.documentElement.scrollTop > 2100) {
+		quinta.classList.add("image");
+	}
+});
+
+>>>>>>> 06f37b1b0691ef9dc1d3097ab35d8e9932c9e029
