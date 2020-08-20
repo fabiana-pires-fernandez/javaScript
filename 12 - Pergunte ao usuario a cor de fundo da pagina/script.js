a9b4074f708eab19a4401b0eb2e0fe966a4cbd42
fabiana-pires-fernandez <fabi.nandez@hotmail.com>
2020-08-20 20:38:53 -0300

    Pergunte ao usuario a cor de fundo da pagina

---
+++ 12 - Pergunte ao usuario a cor de fundo da pagina/script.js
@@ -0,0 +1,22 @@
+<<<<<<< HEAD
+let cor = prompt("Digite a cor: verde ou cinza");
+
+if (cor == "verde") {
+	document.body.style.backgroundColor = "green";
+} else if (cor == "cinza") {
+	document.body.style.backgroundColor = "gray";
+} else {
+	alert("Não temos essa cor");
+	document.location.reload();
+=======
+let cor = prompt("Digite a cor: verde ou cinza");
+
+if (cor == "verde") {
+	document.body.style.backgroundColor = "green";
+} else if (cor == "cinza") {
+	document.body.style.backgroundColor = "gray";
+} else {
+	alert("Não temos essa cor");
+	document.location.reload();
+>>>>>>> 06f37b1b0691ef9dc1d3097ab35d8e9932c9e029
+}
