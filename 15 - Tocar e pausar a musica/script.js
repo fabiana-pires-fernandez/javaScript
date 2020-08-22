848cbe6651bab7af3ffd3dc61ae8ce914af667ae
fabiana-pires-fernandez <fabi.nandez@hotmail.com>
2020-08-22 15:44:21 -0300

    Tocar e pausar a musica

---
+++ 15 - Tocar e pausar a musica/script.js
@@ -0,0 +1,23 @@
+<<<<<<< HEAD
+function tocar() {
+	let toque = document.getElementById("audio");
+	toque.play()
+}
+
+function pause(){
+	let naoToque = document.getElementById("audio");
+	naoToque.pause()
+}
+
+=======
+function tocar() {
+	let toque = document.getElementById("audio");
+	toque.play()
+}
+
+function pause(){
+	let naoToque = document.getElementById("audio");
+	naoToque.pause()
+}
+
+>>>>>>> 06f37b1b0691ef9dc1d3097ab35d8e9932c9e029
